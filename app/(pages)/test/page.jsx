8a2b5adf9247b61a4d8e59989a83e5ff0095dac9
jkclyde31 <randal.technodream@gmail.com>
2024-08-28 12:06:23 +0800

    Update page.jsx

--- app/(pages)/test/page.jsx
+++ app/(pages)/test/page.jsx
@@ -1,38 +1,10 @@
-"use client";
-import React, { useTransition, useState } from "react";
-import Image from "next/image";
+import React from 'react'
 
-
-
-const Test = () => {
-  const [tab, setTab] = useState("skills");
-  const [isPending, startTransition] = useTransition();
-
-  const handleTabChange = (id) => {
-    startTransition(() => {
-      setTab(id);
-    });
-  };
-
+export const page = () => {
   return (
-    <section className="text-black" id="about">
-      <div className="md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
-        <Image src="/img/logo.png" width={300} height={300} className="mx-auto"/>
-        <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
-          <h2 className="text-4xl font-bold text-black mb-4">About Me</h2>
-          <p className="text-base lg:text-lg">
-            I'm a web developer who's passionate about building interactive and
-            responsive web applications. I've worked with various front-end
-            technologies like JavaScript, React, Redux, Bootstrap, Tailwind, and
-            CSS. On the back-end, I have experience with PHP, Node.js, Next.js,
-            and Django. I'm continuously learning and eager to expand my skills,
-            and I'm looking forward to collaborating with others to create great
-            applications.
-          </p>
-        </div>
-      </div>
-    </section>
-  );
-};
+    <div>page</div>
+  )
+}
+
 
 export default Test;
